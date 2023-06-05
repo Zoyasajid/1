722965f2acb0807be0cbfe93a8edf3767c25dcaf
Zoyasajid <sajidzoya72@gmail.com>
2023-06-05 19:07:14 +0500

    first commit

---
+++ type.js
@@ -0,0 +1,50 @@
+const item= document.querySelector("#item")
+const toDoBox= document.querySelector("#to-do-box")
+
+item.addEventListener(
+    "keyup",
+    function(event){
+        if (event.key =="Enter"){
+            addToDo(this.value)
+            this.value= ""
+        }
+    }
+)
+const addToDo = (item) => {
+    const listItem =document.createElement("li");
+    listItem.innerHTML= `
+    ${item}
+    <i class="fa fa-close" style="font-size:36px"></i>
+    
+    `;
+    const listItem2 =document.createElement("li");
+    listItem.innerHTML= `
+    ${item}
+    <i class="fa fa-edit"></i>
+    `;
+
+
+    listItem.addEventListener(
+        "click",
+        function(){
+            this.classList.toggle("done")
+        }
+    )
+
+    listItem.querySelector("i").addEventListener(
+        "click",
+        function(){
+            listItem.remove()
+        }
+    )
+    listItem.querySelector("i")
+        "click",
+        function(){
+        listItem.contentEditable()
+        }
+            
+        const paragraph = document.getElementById("edit");
+    
+
+    toDoBox.appendChild(listItem)
+}
